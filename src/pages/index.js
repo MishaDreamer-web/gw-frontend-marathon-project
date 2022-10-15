6c672958fc2dae84e6bdea4267c2d232e21eab9a
Maryna Ostapliuk <marynaum01@gmail.com>
2022-10-15 18:41:03 +0300

    got the data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,46 +1,44 @@
 import * as React from "react";
-// import { useState } from "react";
+import { useState } from "react";
 import Layout from "../components/Layout/Layout";
-// import { graphql } from "gatsby";
+import { graphql } from "gatsby";
 
-const IndexPage = () => {
-  // const IndexPage = ({ data }) => {
-  // const days = data.allMarkdownRemark.nodes;
-  // const [openedDayId, setOpenedDayId] = useState(days[0].id || 0);
-  // const [lang, setLang] = useState("ua");
-
+// const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  const days = data.allMarkdownRemark.nodes;
+  const [openedDayId, setOpenedDayId] = useState(days[0].id || 0);
+  const [lang, setLang] = useState("ua");
+  console.log(days);
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
-
-      {/* <ul>
+      <ul>
         {days
           ? days?.map(({ id, frontmatter }) => {
               return (
-                <li key={id}>
+                <li key={frontmatter.title}>
                   <button onClick={() => setOpenedDayId(id)}>
-                    {frontmatter.chapter}
+                    {frontmatter.title}
                   </button>
                 </li>
               );
             })
           : null}
       </ul>
-
       <ul>
         {days
           ? days
               ?.find((day) => openedDayId === day.id)
               ?.frontmatter?.subhead?.map(
-                ({ Subhead_title, questions }, index) => {
+                ({ subhead_title, questions }, index) => {
                   return (
                     <div key={index}>
-                      <h3>{Subhead_title}</h3>
+                      <h3>{subhead_title}</h3>
                       <ul key={index}>
                         {questions.map((question, index) => {
                           return (
                             <li key={index}>
-                              <h2>{question.title}</h2>
+                              <h2>{question.question_title}</h2>
                               <p>{question.description}</p>
                             </li>
                           );
@@ -51,14 +49,36 @@ const IndexPage = () => {
                 }
               )
           : null}
-      </ul> */}
+      </ul>
     </Layout>
   );
 };
 
 export default IndexPage;
 
-// export const query = graphql`
+export const query = graphql`
+  query MyQuery {
+    allMarkdownRemark(filter: { frontmatter: { language: { eq: "ua" } } }) {
+      nodes {
+        frontmatter {
+          chapter
+          language
+          title
+          subhead {
+            subhead_title
+            questions {
+              question_title
+              description
+            }
+          }
+        }
+        id
+      }
+    }
+  }
+`;
+
+// export const query = graphql`;
 //   query MainPage {
 //     allMarkdownRemark(filter: { frontmatter: { language: { eq: "ua" } } }) {
 //        nodes {
